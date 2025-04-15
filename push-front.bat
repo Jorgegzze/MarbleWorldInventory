8047ac4aef9f6edca97af3d7bc3a564469e2671a
Jorgegzze <jge@marbleworld.com>
2025-04-15 15:45:08 -0600

    Auto commit

--- push-front.bat
+++ push-front.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d "C:\Users\Jorge\Desktop\marbleworldinventory"
+git config --global user.name "Jorgegzze"
+git config --global user.email "jge@marbleworld.com"
+
 git add .
 git commit -m "Auto commit"
 git push
