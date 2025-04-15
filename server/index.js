a103cbb71b778cd66e6b11231635f1d3960bde61
Jorgegzze <jge@marbleworld.com>
2025-04-15 15:47:15 -0600

    Auto commit

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
+const path = require('path');
 const inventoryRoutes = require('./routes/inventory');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static('public'));
+
+// Routes
 app.use('/inventory', inventoryRoutes);
 
+// Root
+app.get('/', (req, res) => {
+  res.send('MarbleWorldInventory API is running.');
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
